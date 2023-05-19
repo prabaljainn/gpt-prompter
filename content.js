a0c59ead4c12caad1eed5509fbac2c633b4bf03d
Anushka Pateriya <prabaljainn@gmail.com>
2023-05-20 03:39:53 +0530

    🦸‍♂️🦸‍♂️ something is up atleast

--- content.js
+++ content.js
@@ -1,6 +1,21 @@
+let highlightedElement;
+
+let mouseoverHandler = function (event) {
+  highlightedElement = event.target;
+  highlightedElement.style.outline = "1px solid lightcoral"; // change outline color
+};
+
+let mouseoutHandler = function (event) {
+  if (highlightedElement) {
+    highlightedElement.style.outline = ""; // remove outline
+  }
+};
+
 let clickHandler = function (event) {
   let element = event.target;
   let text = element.textContent;
+  // element.style.outline = ""; // remove outline on click
+
   chrome.runtime.sendMessage(
     { command: "addSnippet", snippet: text },
     function (response) {
@@ -11,8 +26,15 @@ let clickHandler = function (event) {
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.command === "start") {
+    document.addEventListener("mouseover", mouseoverHandler);
+    document.addEventListener("mouseout", mouseoutHandler);
     document.addEventListener("click", clickHandler);
   } else if (request.command === "stop") {
+    if (highlightedElement) {
+      highlightedElement.style.outline = ""; // remove outline
+    }
+    document.removeEventListener("mouseover", mouseoverHandler);
+    document.removeEventListener("mouseout", mouseoutHandler);
     document.removeEventListener("click", clickHandler);
   }
 });
--- popup.js
+++ popup.js
@@ -44,3 +44,17 @@ document.getElementById("reset").addEventListener("click", function () {
     paintSnippets();
   });
 });
+
+document.getElementById("gpt").addEventListener("click", function () {
+  chrome.windows.create({
+    url: "https://chat.openai.com/",
+  });
+  const area = document.querySelector("#prompt-textarea");
+  area.innerText =
+    "Hello, I'm a bot that can help you with your questions. Ask me anything!";
+  const btn = document
+    .getElementsByClassName(
+      "absolute p-1 rounded-md text-gray-500 bottom-1.5"
+    )[0]
+    .click();
+});
